9322774fe417f406fca2c1b4247bf3ff9d0128e3
Ge0gN0te <ge0g.n0terdamskyi@proton.me>
2024-03-23 21:58:50 +0200

    Comment help output function

--- include/output.hpp
+++ include/output.hpp
@@ -11,7 +11,7 @@ void OutputBinaryArray(vector<int>& bin_array, string colors[]);
 void OutputHexadecimalArray(vector<int>& hex_array, const char hex_map[], string colors[]);
 void OutputDecimalNumber(int dec_number, string colors[]);
 
-void OutputHelpTxt(string colors[]);
+//void OutputHelpTxt(string colors[]);
 void OutputVersionTxt(string colors[]);
 
 #endif 
