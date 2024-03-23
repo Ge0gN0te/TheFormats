34054294f5b3aa8038dc1fae7229107989328f20
Ge0gN0te <ge0g.n0terdamskyi@proton.me>
2024-03-23 21:06:33 +0200

    update names and arguments for functions

--- include/binary_arithmetic.hpp
+++ include/binary_arithmetic.hpp
@@ -7,10 +7,8 @@
 using namespace std;
 
 // Знаходження суми двох бінарних масивів
-void HowArrayMaxSize(vector<int> first_array, vector<int> second_array, int& max_size);
-void InArray(vector<int> in_array, vector<int> with_array);
-void AdditionBinary(vector<int> first_bin, vector<int> second_bin, vector<int>& result_bin);
-void SubtractionBinary(vector<int> first_bin, vector<int> second_bin, vector<int>& result_bin);
-void MultiplycationBinary(vector<int> first_bin, vector<int> second_bin, vector<int>& result_bin);
+vector<int> AdditionBinary(vector<int> first_bin, vector<int> second_bin);
+vector<int> SubtractionBinary(vector<int> first_bin, vector<int> second_bin);
+vector<int> MultiplycationBinary(vector<int> first_bin, vector<int> second_bin);
 
 #endif 
