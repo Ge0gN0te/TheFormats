bc41f83522ed632c012401bcb779d8fa1ac7c6e9
Ge0gN0te <ge0g.n0terdamskyi@proton.me>
2024-03-27 15:01:45 +0200

    delete divion functions

--- src/binary_arithmetic/binary_arithmetic.cpp
+++ src/binary_arithmetic/binary_arithmetic.cpp
@@ -17,70 +17,48 @@ int HowArraysMaxSize(vector<int> first_array, vector<int> second_array) {
 
 // Знаходження суми двох бінарних масивів
 vector<int> AdditionBinary(vector<int> first_bin, vector<int> second_bin) {
-	int sum = 0;
-	int carry = 0;
+    int sum = 0;
+    int carry = 0;
     int max_size;
-	vector<int> result_bin;
+    vector<int> result_bin;
 
-	max_size = HowArraysMaxSize(first_bin, second_bin);
+    max_size = HowArraysMaxSize(first_bin, second_bin);
 
-	for(int i = 0; i <= max_size - 1; i++) {
-		if(first_bin[i] < 0 || first_bin[i] > 1) {
-			sum = second_bin[i] + carry;
-		} else if(second_bin[i] < 0 || second_bin[i] > 1) {
-			sum = first_bin[i] + carry;
-		} else {
-			sum = first_bin[i] + second_bin[i] + carry;
-		};
+    for(int i = 0; i <= max_size - 1; i++) {
+        sum = first_bin[i] + second_bin[i] + carry;
+        result_bin.push_back(sum % 2);
+        carry = sum / 2;
+    };
 
-		result_bin.push_back(sum % 2);
-		carry = sum / 2;
-	};
+    if(carry > 0) {
+        result_bin.push_back(carry);
+    };
 
-	if(carry > 0) {
-		result_bin.push_back(carry);
-	};
-
-	return result_bin;
+    return result_bin;
 };
 
 vector<int> SubtractionBinary(vector<int> first_bin, vector<int> second_bin) {
-	int diff = 0;
+    int diff = 0;
     int borrow = 0;
-	int max_size;
+    int max_size;
+    vector<int> result_bin;
 
-	vector<int> result_bin;
+    max_size = HowArraysMaxSize(first_bin, second_bin);
 
-	max_size = HowArraysMaxSize(first_bin, second_bin);
-
-    // Цикл для перебору двох бінарних масивів
-	for(int i = 0; i <= max_size - 1; i++) {
-		if(first_bin[i] > 1) {
-			diff = second_bin[i] - borrow;
-		} else if(second_bin[i] > 1) {
-			diff = first_bin[i] - borrow;
-		} else {
-			diff = first_bin[i] - second_bin[i] - borrow;
-		};
+    for(int i = 0; i <= max_size - 1; i++) {
+        diff = first_bin[i] - second_bin[i] - borrow;
 
         if(diff < 0) {
             diff += 2;
             borrow = 1;
-		} else {
+        } else {
             borrow = 0;
-		};
+        };
 
         result_bin.push_back(diff);
-	};
+    };
 
-    // Видалення зайвих нулів з початку результату
-    if(result_bin.size() > 1) {
-		for(int i = 0; result_bin[i] == 0; i++) {
-        	result_bin.erase(result_bin.begin(), result_bin.begin());
-		};
-	};
-
-	return result_bin;
+    return result_bin;
 };
 
 vector<int> MultiplycationBinary(vector<int> first_bin, vector<int> second_bin) {
@@ -91,10 +69,10 @@ vector<int> MultiplycationBinary(vector<int> first_bin, vector<int> second_bin) 
         if (second_bin[i] == 1) {
             // Додавання temp_bin до result_bin, емулюючи множення числа на само себе
             result_bin = AdditionBinary(result_bin, temp_bin);
-        }
+        };
         // Перемноження temp_bin на 10 (додавання нуля зліва)
         temp_bin.insert(temp_bin.begin(), 0);
-    }
+    };
 
     return result_bin;
 };
