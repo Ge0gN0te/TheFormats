d0a48e616697a001ac7c1dfd2642dd9a7c19a36c
Ge0gN0te <ge0g.n0terdamskyi@proton.me>
2024-03-27 14:54:27 +0200

    delete build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/david/Documents/College/ComputerCircuity/LaboratoryWorkN3.4-3.6
-# Build directory: /home/david/Documents/College/ComputerCircuity/LaboratoryWorkN3.4-3.6
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
